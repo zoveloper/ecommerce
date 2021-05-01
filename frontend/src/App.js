41435777288782cf3d600e6e78dc5f688dcfc976
Martin Cho <zoveloper@gmail.com>
2021-04-30 21:56:25 -0400

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import SideNav from './components/SideNav'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <SideNav/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/SideNav.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+
+function SideNav() {
+    return (
+        <div className="sidenav">
+            <a href="http://localhost:3000/"><img src="logo192.png" className="App-logo" alt="logo" /></a>
+            <a href="http://localhost:3000/">WORK</a>
+            <a href="http://localhost:3000/">SHOP</a>
+            <a href="http://localhost:3000/">About</a>
+        </div>
+    )
+}
+
+export default SideNav
