3a365551964d70e29bda8d4589c29199963e33c0
Martin Cho <zoveloper@gmail.com>
2021-05-04 16:23:01 -0400

    Added new side Nav

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,34 +1,15 @@
 import React from 'react';
-import SidebarComponent from './components/SidebarComponent'
+import Sidebar from './components/Sidebar'
 import './App.css';
-import { Row } from 'simple-flexbox';
-import { StyleSheet, css } from 'aphrodite';
 
-const styles = StyleSheet.create({
-  container: {
-      height: '100vh'
-  },
-  content: {
-      marginTop: 54
-  },
-  mainBlock: {
-      backgroundColor: '#F7F8FC',
-      padding: 30
-  }
-});
-class App extends React.Component {
 
-  state = { selectedItem: 'Tickets' };
+function App() {
 
-  render() {
-      const { selectedItem } = this.state;
-      return (
-          <Row className={css(styles.container)}>
-              <SidebarComponent selectedItem={selectedItem} onChange={(selectedItem) => this.setState({ selectedItem })} />
-          </Row>
-      );
-  }
+  return(
+    <div className="App">
+    <Sidebar />
+    </div>
+  );
 }
-
 
 export default App;
