c03ef43ee10670c3a045246a1657c0382d85de6f
Martin Cho <zoveloper@gmail.com>
2021-05-08 23:32:47 -0400

    added sidebar background img

--- frontend/src/components/Sidebar/Sidebar.styles.js
+++ frontend/src/components/Sidebar/Sidebar.styles.js
@@ -2,7 +2,15 @@ import styled from '@emotion/styled'
 
 export const SidebarContainer = styled.div`
     width: 20%;
+    max-Width: 280px;
+    min-Width: 80px;
     color: blue;
-    background: yellow;
-
+    background-image: linear-gradient(
+        315deg, 
+        rgba(255,255,255,0.1) 0%,
+        #eec0c6 74%),
+        url('images/coffee.jpg');
+    background-size:cover ;
+    background-repeat: no repeat;
+    background-position:center center;
 `
