84227456640959efd64dfb4ffc142df92ed330a6
Martin Cho <zoveloper@gmail.com>
2021-05-11 23:34:50 -0400

    added sidebar toggler

--- frontend/src/components/Sidebar/Sidebar.js
+++ frontend/src/components/Sidebar/Sidebar.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React,{useState} from 'react'
 import * as s from './Sidebar.styles'
 
 const Sidebar = props =>{
@@ -7,22 +7,37 @@ const Sidebar = props =>{
         SidevarHeader='', 
         menuItems=[] 
     } = props;
-    
+
+    // state
+    const[selected, setSelectedMenuItem] = useState(menuItems[0].name);
+    const handleMenuItemClick= name => {
+        setSelectedMenuItem(name)
+    }
+
+
+
     const menuItemsJSX = menuItems.map((item, index) =>{
+        const isItemSelected = selected === item.name;
+
         return(
-        <s.MenuItem key={index}>
-            <s.Text>
-                {item.name}
-            </s.Text>
+        <s.MenuItem 
+        key={index}
+        selected={isItemSelected}
+        onClick={() => handleMenuItemClick(item.name)}
+
+        >
+            <s.Text>{item.name} </s.Text>
         </s.MenuItem>
         )
-    })
-
+    });
 
     return(
     <s.SidebarContainer backgroundImage={ backgroundImage }>
-    <s.SidebarHeader>{SidevarHeader}</s.SidebarHeader>
-    <s.MenuItemContainer>{menuItemsJSX}</s.MenuItemContainer>
+        <s.SidebarHeader>{SidevarHeader}</s.SidebarHeader>
+        <s.MenuItemContainer>{menuItemsJSX}</s.MenuItemContainer>
+        <s.TogglerContainer>
+            <s.Toggler />
+        </s.TogglerContainer>
     </s.SidebarContainer>
     )
 }
--- frontend/src/components/Sidebar/Sidebar.styles.js
+++ frontend/src/components/Sidebar/Sidebar.styles.js
@@ -9,10 +9,12 @@ export const SidebarContainer = styled.div`
         315deg, 
         rgba(255,255,255,0.1) 0%,
         #eec0c6 74%),
-        url(${props => props.backgroundImage});
+        url(${p => p.backgroundImage});
     background-size:cover ;
     background-repeat: no repeat;
     background-position:center center;
+
+    position: relative; // Toggler
 `
 
 export const SidebarHeader = styled.h3`
@@ -29,18 +31,63 @@ export const MenuItem =styled.div`
     text-align:center;
     padding: 6px 10px;
     font-weight:600;
-    color: rgba(19 , 15 ,64);
+    color: ${p => p.selected ? ' rgba(225, 225, 225)' : ' rgba(19, 15, 64)'};
+    &:hover{
+        color:rgba(225, 225, 225);
+        transition: .1s ease-in all;
+    }
 
     &:after { 
         content:'';
-        border: 1px solid rgba(225, 112, 85);
+        border: 1px solid ${p => p.selected ? ' rgba(225, 225, 225)' : ' rgba(225, 112, 85)'};
         display: block;
         margin: 8px 0 4px;
-    }
+    };
+
+    ${p =>!p.selected &&`
+        &:hover{
+            &:after{
+                border:1px solid rgba(225, 225, 225,0.2);
+                transition: .1s ease-in all;
+            }
+        }
+    
+    `}
 
 `;
 
 export const Text =styled.p`
     display: inline
+
+`
+//Toggler ------------------------
+
+export const TogglerContainer = styled.div`
+    position: absolute;
+    width: 30%;
+    bottom:10%;
+    left: 0;
+    right: 0;
+    margin: 0 auto;
+
+`
+export const Toggler = styled.div`
+    height: 40px;
+    cursor: pointer;
+    position: relative;
+
+    &:after { 
+        content:'';
+        position: absolute;
+        left: 0;
+        top: .25em;
+        width: 100%;
+        height: .1em;
+        background: #fff;
+        box-shadow:
+        0 .75em 0 0 #fff,
+        0 1.5em 0 0 #fff;
+    };
+    
 
 `
